a52dff0ce6ef33c46d0737511270d4024c747498
Nganie <ngan.tp.quan@gmail.com>
2018-05-06 01:29:29 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+name = ["Edsger, Ada, Charles, Alan, Grace, Linus, and Matz."]
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end
+
+def batch_badge_creator
+
+end
+
+def assign_rooms
+
+end
